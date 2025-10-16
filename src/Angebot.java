7ebac67a830b8e26127b391a9c48e6d2fd70dfc0
Mark <mark.t@gmx.at>
2025-10-16 10:35:27 +0200

    Klassen erstellt und Angebot gemacht

---
+++ src/Angebot.java
@@ -0,0 +1,50 @@
+import java.util.GregorianCalendar;
+
+public class Angebot {
+double regulärpreis;
+GregorianCalendar flugdatum;
+String flugnummer;
+
+    public Angebot(double regulärpreis, GregorianCalendar flugdatum, String flugnummer) {
+        this.regulärpreis = regulärpreis;
+        this.flugdatum = flugdatum;
+        this.flugnummer = flugnummer;
+    }
+
+    public double getRegulärpreis() {
+        return regulärpreis;
+    }
+
+    public void setRegulärpreis(double regulärpreis) {
+        this.regulärpreis = regulärpreis;
+    }
+
+    public GregorianCalendar getFlugdatum() {
+        return flugdatum;
+    }
+
+    public void setFlugdatum(GregorianCalendar flugdatum) {
+        this.flugdatum = flugdatum;
+    }
+
+    public String getFlugnummer() {
+        return flugnummer;
+    }
+
+    public void setFlugnummer(String flugnummer) {
+        this.flugnummer = flugnummer;
+    }
+
+    public void rabattstrategieWählen(){
+
+    }
+
+    @Override
+    public String toString() {
+        return "Angebot{" +
+                "regulärpreis=" + regulärpreis +
+                ", flugdatum=" + flugdatum +
+                ", flugnummer='" + flugnummer + '\'' +
+                '}';
+    }
+}
---
+++ src/Rabattstrategie.java
@@ -0,0 +1,3 @@
+public interface Rabattstrategie {
+    public double getPreis(double preisOhneNachlass);
+}
